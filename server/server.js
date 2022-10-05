a8abbd5d600f5ef083df37c4aeddea1ad352dc96
Summer Villeneuve <svilleneuve1994@gmail.com>
2022-10-04 23:36:15 -0400

    defined path

--- server/server.js
+++ server/server.js
@@ -286,9 +286,14 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
 
 app.use(cors());
 app.use(express.json());
