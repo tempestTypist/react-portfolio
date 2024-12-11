0a0abfa317814cb15f51d6bd5f2af14758d97396
Summer Villeneuve <svilleneuve1994@gmail.com>
2024-12-11 01:25:23 -0500

    nvmrc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import dotenv from 'dotenv';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import reportWebVitals from './reportWebVitals';
-
-require(dotenv).config();
 
 const container = document.getElementById('root');
 const root = createRoot(container);
